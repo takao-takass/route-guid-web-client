ac6c92d659d1601815e25a755acf205cdcb08be1
TakahiroTada <takass0119@yahoo.co.jp>
2023-09-10 23:44:10 +0900

    remove logo mark.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
   <router-view/>
   <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   <!--<RouteList/>-->
